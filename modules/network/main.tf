d0a2525383537fe53e122702894e6283b5c04de3
saurav0401 <142498871+saurav0401@users.noreply.github.com>
2024-05-28 23:36:24 +0530

    Update main.tf

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -65,13 +65,27 @@ resource "aws_subnet" "private_subnet_2" {
   }
 }
 
-# Route Table
+# Public Route Table
 resource "aws_route_table" "public_route_table" {
   vpc_id = aws_vpc.my_vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.my_igw.id
+  }
+
+  tags = {
+    Name = var.public_route_table_name
+  }
+}
+
+# Private Route Table
+resource "aws_route_table" "private_route_table" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.main.id
   }
 
   tags = {
@@ -96,10 +110,21 @@ resource "aws_eip" "my_eip" {
 # Route Table Association for Private Subnets
 resource "aws_route_table_association" "private_subnet_1_association" {
   subnet_id      = aws_subnet.private_subnet_1.id
-  route_table_id = aws_route_table.public_route_table.id
+  route_table_id = aws_route_table.private_route_table.id
 }
 
 resource "aws_route_table_association" "private_subnet_2_association" {
   subnet_id      = aws_subnet.private_subnet_2.id
+  route_table_id = aws_route_table.private_route_table.id
+}
+
+# Route Table Association for Public Subnets
+resource "aws_route_table_association" "public_subnet_1_association" {
+  subnet_id      = aws_subnet.public_subnet_1.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+resource "aws_route_table_association" "public_subnet_2_association" {
+  subnet_id      = aws_subnet.public_subnet_2.id
   route_table_id = aws_route_table.public_route_table.id
 }
