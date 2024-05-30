8439ee683449463939c324bc285c7c6a26a1d025
saurav0401 <142498871+saurav0401@users.noreply.github.com>
2024-05-30 16:15:51 +0530

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,3 +1,6 @@
+module network {
+  source = "../network"
+}
 resource "aws_instance" "ec2_instance" {
 
   ami                         = var.ami
@@ -6,7 +9,7 @@ resource "aws_instance" "ec2_instance" {
   key_name                    = aws_key_pair.my_key_pair.key_name
   monitoring                  = false
   associate_public_ip_address = false
-  vpc_security_group_ids      = [var.security_group_id]
+  vpc_security_group_ids      = [aws_security_group.instance_security_group.id]
   tenancy                     = "default"
   subnet_id                   = var.subnet_id
   tags = {
@@ -37,5 +40,30 @@ resource "aws_key_pair" "my_key_pair" {
 
   provisioner "local-exec" {
     command = "echo '${tls_private_key.private_ssh_key.private_key_pem}' > ../${var.key_pair}.pem"
+  }
+}
+resource "aws_security_group" "instance_security_group" {
+  name        = var.security_group_name
+  description = var.security_group_description
+  vpc_id      = module.network.vpc_id
+  dynamic "ingress" {
+    for_each = var.ingress_rules
+    content {
+      description = ingress.value.description
+      from_port   = ingress.value.from_port
+      to_port     = ingress.value.to_port
+      protocol    = ingress.value.protocol
+      cidr_blocks = ingress.value.cidr_blocks
+    }
+  }
+  dynamic "egress" {
+    for_each = var.egress_rules
+    content {
+      description = egress.value.description
+      from_port   = egress.value.from_port
+      to_port     = egress.value.to_port
+      protocol    = egress.value.protocol
+      cidr_blocks = egress.value.cidr_blocks
+    }
   }
 }
